69d5f127adef91027e193c46ca943666227bd7fb
Zineb2a <zineb.alaoui663@gmail.com>
2023-10-08 17:49:46 -0400

    Added FeaturedProperties section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {LandingPage} from './Landing component/LandingPage';
 import {OurFeature} from './Feature component';
+import {FeaturedProperties} from './FeaturedProperties'
 
 const App = () => {
   return (
     <div className="app">
       <LandingPage />
       <OurFeature />   
+      <FeaturedProperties />   
     </div>
   );
 };
