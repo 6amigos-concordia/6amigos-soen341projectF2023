630abd0a72ab5497f13fd0950ba79fa5d7599cf4
Zineb2a <zineb.alaoui663@gmail.com>
2023-10-08 18:15:55 -0400

    added to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {LandingPage} from './Landing component/LandingPage';
 import {OurFeature} from './Feature component';
 import {FeaturedProperties} from './FeaturedProperties'
 import {Team} from './FeaturedBroker'
+import {Cta} from './newsletter'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <OurFeature />   
       <FeaturedProperties/>   
       <Team/>   
+      <Cta/>
     </div>
   );
 };
