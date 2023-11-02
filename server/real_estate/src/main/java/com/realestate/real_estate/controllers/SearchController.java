6f802505a6c3357d300b3abb3826934c1165ff7f
Jainam Shah <jainammshah12@gmail.com>
2023-11-02 18:23:35 -0400

    commit message

--- server/real_estate/src/main/java/com/realestate/real_estate/controllers/SearchController.java
+++ server/real_estate/src/main/java/com/realestate/real_estate/controllers/SearchController.java
@@ -7,24 +7,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.realestate.real_estate.repos.Property;
 import com.realestate.real_estate.repos.PropertyDetails;
 import com.realestate.real_estate.services.PropertyService;
 
+@RestController
+@RequestMapping(value = "/api")
 public class SearchController {
     @Autowired
     private PropertyService propertyService;
 
     // Define the route for property search
 
-    @PostMapping("/properties/search")
+    @PostMapping(path = "/search")
     public ResponseEntity<List<PropertyDetails>> searchProperties(@RequestParam int bedrooms, @RequestParam int bathrooms, @RequestParam double minPrice, @RequestParam double maxPrice, @RequestParam String district) {
         return ResponseEntity.ok().body(propertyService.searchProperties(bedrooms, bathrooms, minPrice, maxPrice, district));
     }
 
-    @GetMapping("/properties/getAll")
+    @GetMapping(path = "/getAll")
     public ResponseEntity<List<PropertyDetails>> getAllPropertyDetails() {
         return ResponseEntity.ok().body(propertyService.getAllPropertyDetails());
     }
--- server/real_estate/src/main/java/com/realestate/real_estate/services/PropertyService.java
+++ server/real_estate/src/main/java/com/realestate/real_estate/services/PropertyService.java
@@ -3,6 +3,7 @@ package com.realestate.real_estate.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.realestate.real_estate.repos.BrokerRepository;
@@ -10,6 +11,7 @@ import com.realestate.real_estate.repos.Property;
 import com.realestate.real_estate.repos.PropertyDetails;
 import com.realestate.real_estate.repos.PropertyRepository;
 
+@Service
 public class PropertyService {
     @Autowired
     private PropertyRepository propertyRepository;
