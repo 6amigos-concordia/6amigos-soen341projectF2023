25dd825ef1e1889cd11157f3831d793e5fa8f82e
KaotharReda <144386965+KaotharReda@users.noreply.github.com>
2023-11-10 13:55:48 -0500

    implemeted POST api for offer, when listing agent submit and offeran email is sent to the buyer will the offer information as a confirmation

--- server/real_estate/src/main/java/com/realestate/real_estate/repos/Offer.java
+++ server/real_estate/src/main/java/com/realestate/real_estate/repos/Offer.java
@@ -1,2 +1,28 @@
-package com.realestate.real_estate.repos;public class Offer {
+package com.realestate.real_estate.repos;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@Jacksonized
+@Document(collection = "offers")
+public class Offer {
+    @Id
+    ObjectId id;
+    String brokerName;
+    String brokerEmail;
+    String brokerLicenseNum;
+    String Agency;
+    String buyerName;
+    String buyerAddress;
+    String buyerEmail;
+    String addressProperty;
+    Double priceOffered;
+    String deedOfSaleDate;
+    String occupancyDate;
 }
