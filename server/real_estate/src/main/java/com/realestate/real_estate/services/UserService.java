391bff1bbc0a0085cd7c792bcf3652dc8a501aeb
Jainam Shah <98244939+jainammshah12@users.noreply.github.com>
2023-11-09 19:21:12 -0500

    user story 5

--- server/real_estate/src/main/java/com/realestate/real_estate/services/UserService.java
+++ server/real_estate/src/main/java/com/realestate/real_estate/services/UserService.java
@@ -1,17 +1,23 @@
 package com.realestate.real_estate.services;
 
+import com.realestate.real_estate.repos.Broker;
+import com.realestate.real_estate.repos.BrokerRepository;
 import com.realestate.real_estate.repos.User;
 import com.realestate.real_estate.repos.UserRepository;
+
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+    private BrokerRepository brokerRepository;
 
     public List<User> getUsers() {
         return userRepository.findAll();
@@ -19,5 +25,9 @@ public class UserService {
 
     public User saveUser(User user) {
         return userRepository.save(user);
+    }
+
+    public Optional<Broker> getBroker(ObjectId id){
+        return brokerRepository.findById(id);
     }
 }
