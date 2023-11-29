f0e1e97a92f0614316e6f530ec7228c4ed58f270
Zineb2a <zineb.alaoui663@gmail.com>
2023-11-29 00:02:17 -0500

    error handling

--- src/ListingProp/ListingProp.jsx
+++ src/ListingProp/ListingProp.jsx
@@ -62,9 +62,22 @@ useEffect(() => {
       setLoading(false);
     } catch (error) {
       console.error("Error fetching property details:", error);
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.error(error.response.data);
+        console.error(error.response.status);
+        console.error(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        console.error(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.error('Error', error.message);
+      }
       setError(error);
-      setLoading(false);
     }
+    
   };
 
   fetchPropertyDetails();
