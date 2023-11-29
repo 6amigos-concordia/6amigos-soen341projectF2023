0f7d32efee33bd28cbf3db6673afbb9cb88cbdcc
Khanh Huy Nguyen <huy2272@gmail.com>
2023-11-29 18:19:28 -0500

    Implemented endpoint for search using address

--- server/real_estate/src/main/java/com/realestate/real_estate/controllers/SearchController.java
+++ server/real_estate/src/main/java/com/realestate/real_estate/controllers/SearchController.java
@@ -1,9 +1,12 @@
 package com.realestate.real_estate.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
+import com.realestate.real_estate.repos.Broker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,11 +24,12 @@ public class SearchController {
     // Define the route for property search
 
     @PostMapping(path = "/search")
-    public ResponseEntity<List<PropertyDetails>> searchProperties(@RequestParam int bedrooms, @RequestParam int bathrooms, @RequestParam int price, @RequestParam String district,
-    @RequestParam String cityName, 
-    @RequestParam String streetName, 
-    @RequestParam String propertyType) {
-        return ResponseEntity.ok().body(propertyService.searchProperties(bedrooms, bathrooms, price, district, cityName, streetName, propertyType));
+    public ResponseEntity<List<Property>> searchProperties(@RequestBody Property property) {
+        Optional<List<Property>> properties = propertyService.searchProperties(property.getAddress());
+        return properties.map(value -> ResponseEntity.ok().body(value))
+                .orElseGet(() -> {
+                    return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+                });
     }
 
     @GetMapping(path = "/getAll")
