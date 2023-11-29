af306347f63f6e08788e556123b92e590295a700
HAWAAFNANE SAID <hawa21244@gmail.com>
2023-11-29 01:41:36 -0500

    Search Bar logic change

--- src/Search component/Filtercomponent/FilterForm.jsx
+++ src/Search component/Filtercomponent/FilterForm.jsx
@@ -1,59 +1,47 @@
 import React, { useState } from "react";
 
-const FilterFormComponent = ({ onSubmit, onReset }) => {
+const FilterFormComponent = ({ inputValue, onInputChange, onSubmit }) => {
   const [transactionType, setTransactionType] = useState("");
-  const [minPrice, setMinPrice] = useState("");
-  const [maxPrice, setMaxPrice] = useState("");
-  const [minBeds, setMinBeds] = useState("");
-  const [maxBeds, setMaxBeds] = useState("");
-  const [minBaths, setMinBaths] = useState("");
-  const [maxBaths, setMaxBaths] = useState("");
+  const [Price, setPrice] = useState("");
+  const [Beds, setBeds] = useState("");
+  const [Baths, setBaths] = useState("");
   const [districts, setdistricts] = useState("");
-
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (
-      !transactionType &&
-      !minPrice &&
-      !maxPrice &&
-      !minBeds &&
-      !maxBeds &&
-      !minBaths &&
-      !maxBaths &&
-      !districts
-    ) {
-      alert("Please select at least one field before searching.");
-      return;
-    }
-
     const formData = {
       transactionType,
-      minPrice,
-      maxPrice,
-      minBeds,
-      maxBeds,
-      minBaths,
-      maxBaths,
+      Price,
+      Beds,
+      Baths,
       districts,
+      cityName: inputValue.split(',')[0].trim(),
+      streetName: inputValue.split(',')[1]?.trim() || '',
     };
     onSubmit(formData);
   };
 
   const handleReset = () => {
-    // Reset the form data to the initial state
     setTransactionType("");
-    setMinPrice("");
-    setMaxPrice("");
-    setMinBeds("");
-    setMaxBeds("");
-    setMinBaths("");
-    setMaxBaths("");
+    setPrice("");
+    setBeds("");
+    setBaths("");
     setdistricts("");
+    onInputChange("");
   };
 
   return (
     <div className="filter-form">
+      <label> Search: </label>
+      <input
+        className="input-instance"
+        placeholder="City name, street name"
+        value={inputValue}
+        onChange={(e) => onInputChange(e.target.value)}
+      />
+      <br/>
+      <br/>
+      <br/>
       <form onSubmit={handleSubmit}>
         <label>
           Transaction Type:
@@ -61,130 +49,61 @@ const FilterFormComponent = ({ onSubmit, onReset }) => {
             value={transactionType}
             onChange={(e) => setTransactionType(e.target.value)}
           >
-           <option value="">Select</option>
+            <option value="">Select</option>
             <option value="for sale">For Sale</option>
             <option value="for rent">For Rent</option>
             <option value="sold">Sold</option>
-            </select>
-        </label>
-          <label>
-            Min Price:          
-            <select
-            value={minPrice}
-            onChange={(e) => setMinPrice(e.target.value)}
-          >
-            <option value="">Select</option>
-            <option value="500 000$">500 000$</option>
-            <option value="1 000 000">1 000 000$</option>
-            <option value="1 500 000$">1 500 000$</option>
-            <option value="2 000 000$">2 000 000$</option>
-            <option value="2 500 000$">2 500 000$</option>
-            <option value="3 000 000$">3 000 000$</option>
-            <option value="3 500 000$">3 500 000$</option>
-            <option value="4 000 000$">4 000 000$</option>
           </select>
         </label>
         <label>
-          Max Price:
-        <select
-            value={maxPrice}
-            onChange={(e) => setMaxPrice(e.target.value)}
-          >
-            <option value="">Select</option>
-            <option value="500 000$">500 000$"</option>
-            <option value="1 000 000">1 000 000$</option>
-            <option value="1 500 000$">1 500 000$</option>
-            <option value="2 000 000$">2 000 000$</option>
-            <option value="2 500 000$">2 500 000$</option>
-            <option value="3 000 000$">3 000 000$</option>
-            <option value="3 500 000$">3 500 000$</option>
-            <option value="4 000 000$">4 000 000$</option>
-          </select>
-        </label>
-        <label>
-          Min Beds:
+          Price:
           <select
-            value={minBeds}
-            onChange={(e) => setMinBeds(e.target.value)}
+            value={Price}
+            onChange={(e) => setPrice(e.target.value)}
+          >
+            <option value="">Select</option>
+            <option value="500000">$500,000</option>
+            <option value="1000000">$1,000,000</option>
+            <option value="1500000">$1,500,000</option>
+            <option value="2000000">$2,000,000</option>
+            <option value="2500000">$2,500,000</option>
+            <option value="3000000">$3,000,000</option>
+            <option value="3500000">$3,500,000</option>
+            <option value="4000000">$4,000,000</option>
+          </select>
+        </label>
+        <label>
+          Beds:
+          <select
+            value={Beds}
+            onChange={(e) => setBeds(e.target.value)}
           >
             <option value="">Select</option>
             <option value="1">1</option>
-            <option value="1+">1+</option>
             <option value="2">2</option>
-            <option value="2+">2+</option>
             <option value="3">3</option>
-            <option value="3+">3+</option>
-            <option value="4">4 </option>
-            <option value="4+">4+</option>
+            <option value="4">4</option>
             <option value="5">5</option>
-            <option value="5+">5+</option>
             <option value="6">6</option>
           </select>
         </label>
         <label>
-        <label>
-          Max Beds:
+          Baths:
           <select
-            value={maxBeds}
-            onChange={(e) => setMaxBeds(e.target.value)}
+            value={Baths}
+            onChange={(e) => setBaths(e.target.value)}
           >
             <option value="">Select</option>
             <option value="1">1</option>
-            <option value="1+">1+</option>
             <option value="2">2</option>
-            <option value="2+">2+</option>
             <option value="3">3</option>
-            <option value="3+">3+</option>
-            <option value="4">4 </option>
-            <option value="4+">4+</option>
+            <option value="4">4</option>
             <option value="5">5</option>
-            <option value="5+">5+</option>
-            <option value="6">6</option>
-          </select>
-        </label>
-        </label>
-        <label>
-          Min Baths:
-          <select
-            value={minBaths}
-            onChange={(e) => setMinBaths(e.target.value)}
-          >
-            <option value="">Select</option>
-            <option value="1">1</option>
-            <option value="1+">1+</option>
-            <option value="2">2</option>
-            <option value="2+">2+</option>
-            <option value="3">3</option>
-            <option value="3+">3+</option>
-            <option value="4">4 </option>
-            <option value="4+">4+</option>
-            <option value="5">5</option>
-            <option value="5+">5+</option>
             <option value="6">6</option>
           </select>
         </label>
         <label>
-          Max Baths:
-          <select
-            value={maxBaths}
-            onChange={(e) => setMaxBaths(e.target.value)}
-          >
-            <option value="">Select</option>
-            <option value="1">1</option>
-            <option value="1+">1+</option>
-            <option value="2">2</option>
-            <option value="2+">2+</option>
-            <option value="3">3</option>
-            <option value="3+">3+</option>
-            <option value="4">4 </option>
-            <option value="4+">4+</option>
-            <option value="5">5</option>
-            <option value="5+">5+</option>
-            <option value="6">6</option>
-          </select>
-        </label>
-        <label>
-          District
+          District:
           <input
             type="text"
             value={districts}
