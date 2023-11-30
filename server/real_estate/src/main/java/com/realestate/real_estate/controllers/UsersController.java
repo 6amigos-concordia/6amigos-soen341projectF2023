c32e94151f853e22d83df8912466a176c8e962a7
KaotharReda <144386965+KaotharReda@users.noreply.github.com>
2023-11-29 19:26:40 -0500

    deleted class buyer and changed the method sigIn in controller tohhave status 403 when autenthication fails

--- server/real_estate/src/main/java/com/realestate/real_estate/controllers/UsersController.java
+++ server/real_estate/src/main/java/com/realestate/real_estate/controllers/UsersController.java
@@ -3,6 +3,8 @@ package com.realestate.real_estate.controllers;
 import com.realestate.real_estate.repos.User;
 import com.realestate.real_estate.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,7 +21,11 @@ public class UsersController {
     }
 
     @PostMapping("/sign-in")
-    public User signIn(@RequestParam String name, @RequestParam String password) {
-        return userService.signIn(name, password);
+    public ResponseEntity<User> signIn(@RequestParam String name, @RequestParam String password) {
+        if (userService.signIn(name,password) != null){
+            return ResponseEntity.ok().build();
+        }
+        else
+            return new ResponseEntity<>(null, HttpStatus.FORBIDDEN);
     }
 }
